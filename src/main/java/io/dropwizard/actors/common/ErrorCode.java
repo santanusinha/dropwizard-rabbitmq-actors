f5bd303a06b509143e00a6ffa7c88fcac2cb0dff
tushar.mandar <tushar.mandar@flipkart.com>
2016-11-04 12:42:14 +0530

    removed redundant error code

--- src/main/java/io/dropwizard/actors/common/ErrorCode.java
+++ src/main/java/io/dropwizard/actors/common/ErrorCode.java
@@ -4,12 +4,5 @@ package io.dropwizard.actors.common;
  * Error codes
  */
 public enum ErrorCode {
-    DAO_ERROR,
-    JSON_ERROR,
     INTERNAL_ERROR,
-
-    DUPLICATE_DISBURSEMENT,
-
-    CACHE_ERROR, HTTP_ERROR
-
 }
