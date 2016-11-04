486d494c8807fc9b26f629ae3c9ba1034960b01d
tushar.mandar <tusharmndr@gmail.com>
2016-11-04 15:25:45 +0530

    added Json sub type in retry config

--- src/main/java/io/dropwizard/actors/retry/config/RetryConfig.java
+++ src/main/java/io/dropwizard/actors/retry/config/RetryConfig.java
@@ -1,5 +1,6 @@
 package io.dropwizard.actors.retry.config;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.dropwizard.actors.retry.RetryType;
 import lombok.Data;
@@ -10,6 +11,15 @@ import lombok.ToString;
  * Configure a retry strategy
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = CountLimitedExponentialWaitRetryConfig.class),
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = CountLimitedFixedWaitRetryConfig.class),
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = CountLimitedIncrementalWaitRetryConfig.class),
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = NoRetryConfig.class),
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = TimeLimitedExponentialWaitRetryConfig.class),
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = TimeLimitedFixedWaitRetryConfig.class),
+        @JsonSubTypes.Type(name = "PAYMENT_WALLET_TO_WALLET", value = TimeLimitedIncrementalWaitRetryConfig.class)
+})
 @Data
 @EqualsAndHashCode
 @ToString
