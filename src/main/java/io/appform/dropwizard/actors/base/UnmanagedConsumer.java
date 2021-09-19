db5f97d9a7703141292318b13ddb23d3729cfbf8
Deepak Kumar Singh <deepak.kumarsingh@PP-C02FD3D4MD6M.local>
2021-09-20 01:37:09 +0530

    Moved handler to separate class

--- src/main/java/io/appform/dropwizard/actors/base/UnmanagedConsumer.java
+++ src/main/java/io/appform/dropwizard/actors/base/UnmanagedConsumer.java
@@ -2,24 +2,17 @@ package io.appform.dropwizard.actors.base;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
-import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
 import io.appform.dropwizard.actors.actor.ActorConfig;
 import io.appform.dropwizard.actors.actor.MessageHandlingFunction;
-import io.appform.dropwizard.actors.actor.MessageMetadata;
 import io.appform.dropwizard.actors.base.utils.NamingUtils;
 import io.appform.dropwizard.actors.connectivity.RMQConnection;
 import io.appform.dropwizard.actors.exceptionhandler.ExceptionHandlingFactory;
 import io.appform.dropwizard.actors.exceptionhandler.handlers.ExceptionHandler;
 import io.appform.dropwizard.actors.retry.RetryStrategy;
 import io.appform.dropwizard.actors.retry.RetryStrategyFactory;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
 
@@ -38,18 +31,17 @@ public class UnmanagedConsumer<Message> {
     private final RetryStrategy retryStrategy;
     private final ExceptionHandler exceptionHandler;
 
-    private List<Handler> handlers = Lists.newArrayList();
+    private List<Handler<Message>> handlers = Lists.newArrayList();
 
-    public UnmanagedConsumer(
-            String name,
-            ActorConfig config,
-            RMQConnection connection,
-            ObjectMapper mapper,
-            RetryStrategyFactory retryStrategyFactory,
-            ExceptionHandlingFactory exceptionHandlingFactory,
-            Class<? extends Message> clazz,
-            MessageHandlingFunction<Message, Boolean> handlerFunction,
-            Function<Throwable, Boolean> errorCheckFunction) {
+    public UnmanagedConsumer(final String name,
+                             final ActorConfig config,
+                             final RMQConnection connection,
+                             final ObjectMapper mapper,
+                             final RetryStrategyFactory retryStrategyFactory,
+                             final ExceptionHandlingFactory exceptionHandlingFactory,
+                             final Class<? extends Message> clazz,
+                             final MessageHandlingFunction<Message, Boolean> handlerFunction,
+                             final Function<Throwable, Boolean> errorCheckFunction) {
         this.name = NamingUtils.prefixWithNamespace(name);
         this.config = config;
         this.connection = connection;
@@ -63,63 +55,11 @@ public class UnmanagedConsumer<Message> {
         this.exceptionHandler = exceptionHandlingFactory.create(config.getExceptionHandlerConfig());
     }
 
-    private boolean handle(Message message, MessageMetadata messageMetadata) throws Exception {
-        return handlerFunction.apply(message, messageMetadata);
-    }
-
-    private class Handler extends DefaultConsumer {
-
-        private final ObjectMapper mapper;
-        private final Class<? extends Message> clazz;
-
-        @Getter
-        @Setter
-        private String tag;
-
-        private Handler(Channel channel,
-                        ObjectMapper mapper,
-                        Class<? extends Message> clazz,
-                        int prefetchCount) throws Exception {
-            super(channel);
-            this.mapper = mapper;
-            this.clazz = clazz;
-            getChannel().basicQos(prefetchCount);
-        }
-
-        @Override
-        public void handleDelivery(String consumerTag, Envelope envelope,
-                                   AMQP.BasicProperties properties, byte[] body) throws IOException {
-            try {
-                final Message message = mapper.readValue(body, clazz);
-                boolean success = retryStrategy.execute(() -> handle(message, messageProperties(envelope)));
-                if (success) {
-                    getChannel().basicAck(envelope.getDeliveryTag(), false);
-                } else {
-                    getChannel().basicReject(envelope.getDeliveryTag(), false);
-                }
-            } catch (Throwable t) {
-                log.error("Error processing message...", t);
-                if (errorCheckFunction.apply(t)) {
-                    log.warn("Acked message due to exception: ", t);
-                    getChannel().basicAck(envelope.getDeliveryTag(), false);
-                } else if (exceptionHandler.handle()) {
-                    log.warn("Acked message due to exception handling strategy: ", t);
-                    getChannel().basicAck(envelope.getDeliveryTag(), false);
-                } else {
-                    getChannel().basicReject(envelope.getDeliveryTag(), false);
-                }
-            }
-        }
-
-        private MessageMetadata messageProperties(final Envelope envelope) {
-            return new MessageMetadata(envelope.isRedeliver());
-        }
-    }
-
     public void start() throws Exception {
         for (int i = 1; i <= config.getConcurrency(); i++) {
             Channel consumeChannel = connection.newChannel();
-            final Handler handler = new Handler(consumeChannel, mapper, clazz, prefetchCount);
+            final Handler<Message> handler = new Handler<>(consumeChannel, mapper, clazz,
+                    prefetchCount, errorCheckFunction, retryStrategy, exceptionHandler, handlerFunction);
             final String tag = consumeChannel.basicConsume(queueName, false, handler);
             handler.setTag(tag);
             handlers.add(handler);
@@ -127,13 +67,12 @@ public class UnmanagedConsumer<Message> {
         }
     }
 
-    public void stop() throws Exception {
+    public void stop() {
         handlers.forEach(handler -> {
             try {
                 final Channel channel = handler.getChannel();
                 channel.basicCancel(handler.getTag());
-                channel.close();
-                log.info("Consumer channel {} closed.", name);
+                log.info("Consumer channel {} cancelled.", name);
             } catch (Exception e) {
                 log.error(String.format("Error cancelling consumer: %s", handler.getTag()), e);
             }
