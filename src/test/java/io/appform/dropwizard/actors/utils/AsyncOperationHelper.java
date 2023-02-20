ee5687b5d5b44cdd8cdbf469fc87287ffebef37d
Swapnil Gaikwad <swapnil.gaikwad@phonepe.com>
2023-02-20 18:26:12 +0530

    Remove unnessary tests

--- src/test/java/io/appform/dropwizard/actors/utils/AsyncOperationHelper.java
+++ src/test/java/io/appform/dropwizard/actors/utils/AsyncOperationHelper.java
@@ -1,14 +1,10 @@
 package io.appform.dropwizard.actors.utils;
 
-import io.appform.dropwizard.actors.ConnectionRegistry;
 import io.appform.dropwizard.actors.actor.ActorConfig;
 import io.appform.dropwizard.actors.actor.ConsumerConfig;
 import io.appform.dropwizard.actors.actor.ProducerConfig;
-import io.appform.dropwizard.actors.connectivity.RMQConnection;
 import io.appform.dropwizard.actors.connectivity.strategy.SharedConnectionStrategy;
-import io.appform.dropwizard.actors.exceptionhandler.ExceptionHandlingFactory;
 import io.appform.dropwizard.actors.exceptionhandler.config.DropConfig;
-import io.appform.dropwizard.actors.retry.RetryStrategyFactory;
 import io.appform.dropwizard.actors.retry.config.CountLimitedExponentialWaitRetryConfig;
 import io.dropwizard.util.Duration;
 import lombok.Getter;
@@ -18,21 +14,6 @@ import java.util.List;
 
 @Getter
 public class AsyncOperationHelper {
-    private final RMQConnection rmqConnection;
-    private final ConnectionRegistry connRegistry;
-    private final RetryStrategyFactory retryStrategyFactory;
-    private final ExceptionHandlingFactory exceptionHandlingFactory;
-
-    public AsyncOperationHelper(RMQConnection rmqConnection,
-                                ConnectionRegistry connRegistry,
-                                RetryStrategyFactory retryStrategyFactory,
-                                ExceptionHandlingFactory exceptionHandlingFactory) {
-        this.rmqConnection = rmqConnection;
-        this.connRegistry = connRegistry;
-        this.retryStrategyFactory = retryStrategyFactory;
-        this.exceptionHandlingFactory = exceptionHandlingFactory;
-    }
-
     public static ActorConfig buildActorConfig() {
         List<String> routingKey = new ArrayList<>();
         routingKey.add("testsuite");
