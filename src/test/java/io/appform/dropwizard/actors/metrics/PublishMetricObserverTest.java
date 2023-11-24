1231d4e05074186e01e50e770569e088ac93d878
Satyam Saxena <satyam.saxena@phonepe.com>
2023-11-24 14:18:11 +0530

    Add src/main/java/io/appform/dropwizard/actors/base/UnmanagedPublisher.java

---
+++ src/test/java/io/appform/dropwizard/actors/metrics/PublishMetricObserverTest.java
@@ -0,0 +1,84 @@
+package io.appform.dropwizard.actors.metrics;
+
+import com.codahale.metrics.MetricRegistry;
+import io.appform.dropwizard.actors.common.PublishOperations;
+import io.appform.dropwizard.actors.config.MetricConfig;
+import io.appform.dropwizard.actors.config.RMQConfig;
+import io.appform.dropwizard.actors.observers.PublishObserverContext;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PublishMetricObserverTest {
+
+    private PublishMetricObserver publishMetricObserver;
+    private RMQConfig config;
+    private final MetricRegistry metricRegistry = new MetricRegistry();;
+
+    @BeforeEach
+    public void setup() {
+        this.config = RMQConfig.builder()
+                .brokers(new ArrayList<>())
+                .userName("")
+                .threadPoolSize(1)
+                .password("")
+                .secure(false)
+                .startupGracePeriodSeconds(1)
+                .metricConfig(MetricConfig.builder().enabledForAll(true).build())
+                .build();
+        this.publishMetricObserver = new PublishMetricObserver(config, metricRegistry);
+    }
+
+    @Test
+    void testExecuteWhenMetricNotApplicable() {
+        val config = this.config;
+        config.setMetricConfig(MetricConfig.builder().enabledForAll(false).build());
+        val publishMetricObserver = new PublishMetricObserver(config, metricRegistry);
+        assertEquals(terminate(),
+                publishMetricObserver.execute(PublishObserverContext.builder().build(), this::terminate));
+    }
+
+    @Test
+    void testExecuteWithNoException() {
+        val context = PublishObserverContext.builder()
+                .operation(PublishOperations.PUBLISH.name())
+                .queueName("default")
+                .build();
+        assertEquals(terminate(), publishMetricObserver.execute(context, this::terminate));
+        val key = MetricKeyData.builder().operation(context.getOperation()).queueName(context.getQueueName()).build();
+        validateMetrics(publishMetricObserver.getMetricCache().get(key), 1, 0);
+    }
+
+    @Test
+    void testExecuteWithException() {
+        val context = PublishObserverContext.builder()
+                .operation(PublishOperations.PUBLISH.name())
+                .queueName("default")
+                .build();
+        assertThrows(RuntimeException.class, () -> publishMetricObserver.execute(context, this::terminateWithException));
+        val key = MetricKeyData.builder().operation(context.getOperation()).queueName(context.getQueueName()).build();
+        validateMetrics(publishMetricObserver.getMetricCache().get(key), 0, 1);
+    }
+
+    private void validateMetrics(final MetricData metricData,
+                                 final int successCount,
+                                 final int failedCount) {
+        assertEquals(1, metricData.getTotal().getCount());
+        assertEquals(1, metricData.getTimer().getCount());
+        assertEquals(successCount, metricData.getSuccess().getCount());
+        assertEquals(failedCount, metricData.getFailed().getCount());
+    }
+
+    private Integer terminate() {
+        return 1;
+    }
+
+    private Integer terminateWithException() {
+        throw new RuntimeException();
+    }
+}
