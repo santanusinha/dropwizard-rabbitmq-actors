e543f8ad8c2c40e16cc56ca98de227d1bc3c5783
phaneesh <phaneesh.n@gmail.com>
2024-06-01 08:57:32 +0530

    1) Update to DW 3.0.7 2) Update a few dependencies to make it inline with current. 3) Update httpclient to 5.x since DW 3 has migrated to httpclient 5.x

--- src/test/java/io/appform/dropwizard/actors/connectivity/actor/RabbitMQBundleTestAppConfiguration.java
+++ src/test/java/io/appform/dropwizard/actors/connectivity/actor/RabbitMQBundleTestAppConfiguration.java
@@ -1,6 +1,7 @@
 package io.appform.dropwizard.actors.connectivity.actor;
 
-import io.dropwizard.Configuration;
+
+import io.dropwizard.core.Configuration;
 
 public class RabbitMQBundleTestAppConfiguration extends Configuration {
 }
