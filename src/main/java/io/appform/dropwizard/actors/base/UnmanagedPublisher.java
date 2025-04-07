b1c2ae0dc544992e894141b0b27ba135a8c4d8a4
Ankush Nakaskar <ankush.nakaskar@phonepe.com>
2025-04-07 10:47:59 +0530

    Refactored the code

--- src/main/java/io/appform/dropwizard/actors/base/UnmanagedPublisher.java
+++ src/main/java/io/appform/dropwizard/actors/base/UnmanagedPublisher.java
@@ -1,5 +1,8 @@
 package io.appform.dropwizard.actors.base;
 
+import static io.appform.dropwizard.actors.common.Constants.MESSAGE_EXPIRY_TEXT;
+import static io.appform.dropwizard.actors.common.Constants.MESSAGE_PUBLISHED_TEXT;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.rabbitmq.client.AMQP;
@@ -12,18 +15,14 @@ import io.appform.dropwizard.actors.common.RabbitmqActorException;
 import io.appform.dropwizard.actors.connectivity.RMQConnection;
 import io.appform.dropwizard.actors.observers.PublishObserverContext;
 import io.appform.dropwizard.actors.observers.RMQObserver;
-import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.apache.commons.lang3.RandomUtils;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.HashMap;
-
-import static io.appform.dropwizard.actors.common.Constants.MESSAGE_EXPIRY_TEXT;
-import static io.appform.dropwizard.actors.common.Constants.MESSAGE_PUBLISHED_TEXT;
-
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.apache.commons.lang3.RandomUtils;
 @Slf4j
 public class UnmanagedPublisher<Message> {
 
