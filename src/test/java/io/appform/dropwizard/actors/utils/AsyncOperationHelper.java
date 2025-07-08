465712c2a3689d71ab64f197ae15dd20b2cd7321
Nizampuram Lalith Naren <lalithnaren08e@gmail.com>
2025-07-08 15:09:46 +0530

    Added Sideline Processor related tests

--- src/test/java/io/appform/dropwizard/actors/utils/AsyncOperationHelper.java
+++ src/test/java/io/appform/dropwizard/actors/utils/AsyncOperationHelper.java
@@ -6,6 +6,7 @@ import io.appform.dropwizard.actors.actor.ProducerConfig;
 import io.appform.dropwizard.actors.connectivity.strategy.SharedConnectionStrategy;
 import io.appform.dropwizard.actors.exceptionhandler.config.DropConfig;
 import io.appform.dropwizard.actors.retry.config.CountLimitedExponentialWaitRetryConfig;
+import io.appform.dropwizard.actors.retry.config.CountLimitedFixedWaitRetryConfig;
 import io.dropwizard.util.Duration;
 import lombok.Getter;
 
@@ -38,6 +39,40 @@ public class AsyncOperationHelper {
                 .consumer(ConsumerConfig.builder()
                         .connectionIsolationStrategy(SharedConnectionStrategy.builder()
                                 .name(String.join("_", "c", String.join("_", routingKey).toLowerCase()))
+                                .build())
+                        .build())
+                .build();
+    }
+
+    public static ActorConfig buildActorConfigWithSidelineProcessorEnabled() {
+        List<String> routingKey = new ArrayList<>();
+        routingKey.add("testsuite");
+        routingKey.add("first");
+        return ActorConfig.builder()
+                .exchange("test.exchange")
+                .prefix("test")
+                .concurrency(2)
+                .prefetchCount(1)
+                .shardCount(2)
+                .retryConfig(CountLimitedExponentialWaitRetryConfig.builder()
+                        .maxAttempts(1)
+                        .multipier(50)
+                        .maxTimeBetweenRetries(Duration.seconds(30))
+                        .build())
+                .sidelineProcessorConcurrency(1)
+                .producer(ProducerConfig.builder()
+                        .connectionIsolationStrategy(SharedConnectionStrategy.builder()
+                                .name(String.join("_", "p", String.join("_", routingKey).toLowerCase()))
+                                .build())
+                        .build())
+                .consumer(ConsumerConfig.builder()
+                        .connectionIsolationStrategy(SharedConnectionStrategy.builder()
+                                .name(String.join("_", "c", String.join("_", routingKey).toLowerCase()))
+                                .build())
+                        .build())
+                .sidelineProcessor(ConsumerConfig.builder()
+                        .connectionIsolationStrategy(SharedConnectionStrategy.builder()
+                                .name(String.join("_", "s", String.join("_", routingKey).toLowerCase()))
                                 .build())
                         .build())
                 .build();
